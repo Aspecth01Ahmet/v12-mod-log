87a4ee211173983dc4c354773fd3653d457b27ee
Aspecth01Ahmet <76404183+Aspecth01Ahmet@users.noreply.github.com>
2021-06-07 21:20:15 +0300

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,12 @@
+const chalk = require("chalk");
+const moment = require("moment");
+const Discord = require("discord.js");
+const ayarlar = require("../ayarlar.json");
+
+module.exports = client => {
+  console.log(`[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: Aktif, Komutlar Yüklendi!`);
+  console.log(`[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: ${client.user.username} İsmi ile Sisteme Giriş Yapıldı!`);
+  client.user.setStatus("idle");
+  client.user.setActivity("Valiant Developers | 7/24 Hizmette");
+};
+ 
